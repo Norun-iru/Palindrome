f4789a472243d1a9c147036e5b3b49ba6d3b64e7
Norun-iru <97i.issei@gmail.com>
2020-12-02 21:52:23 +0900

    first-commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace kaibunjyenerate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSrc = new System.Windows.Forms.TextBox();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtCheck = new System.Windows.Forms.TextBox();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.lblTrue = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.名前を付けて保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.閉じるToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.色変更ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.背景画像変更ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.excelで保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("源柔ゴシック Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(64, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "回文ジェネレーター";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("源柔ゴシック Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(89, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "原文";
+            this.label2.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("源柔ゴシック Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(536, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "回文";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("源柔ゴシック Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(64, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 38);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "回文チェッカー";
+            // 
+            // txtSrc
+            // 
+            this.txtSrc.Location = new System.Drawing.Point(156, 182);
+            this.txtSrc.Name = "txtSrc";
+            this.txtSrc.Size = new System.Drawing.Size(237, 25);
+            this.txtSrc.TabIndex = 5;
+            this.txtSrc.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(599, 182);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(237, 25);
+            this.txtDest.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("源柔ゴシック Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(89, 371);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "原文";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Location = new System.Drawing.Point(761, 585);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 35);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "クリア";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtCheck
+            // 
+            this.txtCheck.Location = new System.Drawing.Point(156, 371);
+            this.txtCheck.Name = "txtCheck";
+            this.txtCheck.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtCheck.Size = new System.Drawing.Size(680, 25);
+            this.txtCheck.TabIndex = 10;
+            this.txtCheck.TextChanged += new System.EventHandler(this.txtCheck_TextChanged);
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.AutoSize = true;
+            this.lblCheck.Location = new System.Drawing.Point(153, 533);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(0, 17);
+            this.lblCheck.TabIndex = 11;
+            // 
+            // lblTrue
+            // 
+            this.lblTrue.AutoSize = true;
+            this.lblTrue.Font = new System.Drawing.Font("源柔ゴシック Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblTrue.Location = new System.Drawing.Point(152, 528);
+            this.lblTrue.Name = "lblTrue";
+            this.lblTrue.Size = new System.Drawing.Size(234, 22);
+            this.lblTrue.TabIndex = 12;
+            this.lblTrue.Text = "原文に文を入力してください。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("源柔ゴシック Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(89, 457);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "反転";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(156, 457);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(680, 25);
+            this.txtOut.TabIndex = 14;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem,
+            this.設定ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(963, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.名前を付けて保存ToolStripMenuItem,
+            this.excelで保存ToolStripMenuItem,
+            this.閉じるToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 名前を付けて保存ToolStripMenuItem
+            // 
+            this.名前を付けて保存ToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.名前を付けて保存ToolStripMenuItem.Name = "名前を付けて保存ToolStripMenuItem";
+            this.名前を付けて保存ToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl + Shift + S";
+            this.名前を付けて保存ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.名前を付けて保存ToolStripMenuItem.Size = new System.Drawing.Size(244, 22);
+            this.名前を付けて保存ToolStripMenuItem.Text = "名前を付けて保存";
+            this.名前を付けて保存ToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.名前を付けて保存ToolStripMenuItem.ToolTipText = "回文に書いてある回文を保存します。";
+            this.名前を付けて保存ToolStripMenuItem.Click += new System.EventHandler(this.名前を付けて保存ToolStripMenuItem_Click);
+            // 
+            // 閉じるToolStripMenuItem
+            // 
+            this.閉じるToolStripMenuItem.Name = "閉じるToolStripMenuItem";
+            this.閉じるToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl + W";
+            this.閉じるToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.閉じるToolStripMenuItem.Size = new System.Drawing.Size(244, 22);
+            this.閉じるToolStripMenuItem.Text = "閉じる";
+            this.閉じるToolStripMenuItem.Click += new System.EventHandler(this.閉じるToolStripMenuItem_Click);
+            // 
+            // 設定ToolStripMenuItem
+            // 
+            this.設定ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.色変更ToolStripMenuItem,
+            this.背景画像変更ToolStripMenuItem});
+            this.設定ToolStripMenuItem.Name = "設定ToolStripMenuItem";
+            this.設定ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.設定ToolStripMenuItem.Text = "設定";
+            // 
+            // 色変更ToolStripMenuItem
+            // 
+            this.色変更ToolStripMenuItem.Name = "色変更ToolStripMenuItem";
+            this.色変更ToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl + Shift + C";
+            this.色変更ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.C)));
+            this.色変更ToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.色変更ToolStripMenuItem.Text = "色変更";
+            this.色変更ToolStripMenuItem.Click += new System.EventHandler(this.色変更ToolStripMenuItem_Click);
+            // 
+            // 背景画像変更ToolStripMenuItem
+            // 
+            this.背景画像変更ToolStripMenuItem.Name = "背景画像変更ToolStripMenuItem";
+            this.背景画像変更ToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.背景画像変更ToolStripMenuItem.Text = "背景画像変更";
+            this.背景画像変更ToolStripMenuItem.Click += new System.EventHandler(this.背景画像変更ToolStripMenuItem_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(665, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(171, 21);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "真ん中の文字をかぶらせる";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // excelで保存ToolStripMenuItem
+            // 
+            this.excelで保存ToolStripMenuItem.Name = "excelで保存ToolStripMenuItem";
+            this.excelで保存ToolStripMenuItem.Size = new System.Drawing.Size(244, 22);
+            this.excelで保存ToolStripMenuItem.Text = "Excelで保存";
+            this.excelで保存ToolStripMenuItem.Click += new System.EventHandler(this.excelで保存ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(963, 668);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblTrue);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.txtCheck);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.txtSrc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("源柔ゴシック Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "回文ジェネレーター＆チェッカー";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSrc;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtCheck;
+        private System.Windows.Forms.Label lblCheck;
+        private System.Windows.Forms.Label lblTrue;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 色変更ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 名前を付けて保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 閉じるToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolStripMenuItem 背景画像変更ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelで保存ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace kaibunjyenerate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string s, d, t;
+            int i;
+
+            s = txtSrc.Text;
+            if (s.Length > 0)
+            {
+               
+                if(checkBox1.Checked == false)
+                {
+                    d = s;
+                    t = s.Substring(0, s.Length - 1);
+                    for (i = t.Length - 1; i >= 0; i--)
+                    {
+                        d += t.Substring(i, 1);
+                    }
+                    txtDest.Text = d;
+                }
+                if(checkBox1.Checked == true)
+                {
+                    d = s;
+                    d = string.Concat(d.Reverse());
+                    t = s + d;
+                    txtDest.Text = t;
+                }
+                             
+            }
+               if(checkBox1.Checked == true)
+               {
+                   d = s;
+                   d = string.Concat(d.Reverse());
+                   t = s + d;
+                   txtDest.Text = t;
+               }
+            
+
+
+
+
+            if (txtSrc.Text == "")
+            {
+                txtDest.Text = "";
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSrc.Text = "";
+            txtDest.Text = "";
+            txtSrc.Focus();
+            txtCheck.Text = "";
+            txtOut.Text = "";
+
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtCheck_TextChanged(object sender, EventArgs e)
+        {
+            string c, o;
+            c = txtCheck.Text;
+            o = string.Concat(c.Reverse());
+            txtOut.Text = o;
+
+            if (c == o)
+            {
+                lblTrue.Text = "回文です。";
+            }
+            else
+            {
+                lblTrue.Text = "回文ではありません。";
+            }
+
+            if (txtCheck.Text == "")
+            {
+                lblTrue.Text = "原文に文を入力してください。";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           if ( this.BackColor == Color.FromArgb(64,64,64))
+            {
+               this.BackColor = System.Drawing.Color.White;
+               this.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                this.BackColor = System.Drawing.Color.FromArgb(64,64,64);
+                this.ForeColor = System.Drawing.Color.White;
+            }
+            
+        }
+
+        private void 色変更ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor == Color.FromArgb(64, 64, 64))
+            {
+                this.BackColor = System.Drawing.Color.White;
+                this.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                this.BackColor = System.Drawing.Color.FromArgb(64, 64, 64);
+                this.ForeColor = System.Drawing.Color.White;
+            }
+        }
+
+        private void 名前を付けて保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "テキストファイル(*.txt)|*.txt";
+            dialog.Title = "保存";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(dialog.FileName, txtDest.Text);
+            }
+        }
+
+        private void 閉じるToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string s, d, t;
+            int i;
+            s = txtSrc.Text;
+            if(s.Length > 0)
+            {
+               if (checkBox1.Checked == true)
+               {
+                   s = txtSrc.Text;
+                   d = s;
+                   d = string.Concat(d.Reverse());
+                   t = s + d;
+                   txtDest.Text = t;
+               }
+               if (checkBox1.Checked == false)
+               {
+                   s = txtSrc.Text;
+                   d = s;
+                   t = s.Substring(0, s.Length - 1);
+                   for (i = t.Length - 1; i >= 0; i--)
+                   {
+                       d += t.Substring(i, 1);
+                   }
+                   txtDest.Text = d;
+               }
+            
+
+
+            }
+        }
+
+        private void 背景画像変更ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void excelで保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excelブック(*.xlsx)|*.xlsx";
+            dialog.Title = "保存";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(dialog.FileName, txtDest.Text);
+            }
+        }
+    }
+}
